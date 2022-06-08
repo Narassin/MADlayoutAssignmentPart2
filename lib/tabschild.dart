36a122ac71a0bab2abd0ea4a8597e7877948084d
Narassin <37112149+Narassin@users.noreply.github.com>
2022-06-09 03:26:15 +0800

    Finalized the tabviews

--- lib/tabschild.dart
+++ lib/tabschild.dart
@@ -9,19 +9,19 @@ class TabSon extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
         child: TabBarView(
-      children: [GridBox(), Text('text'), Text('text')],
+      children: [AllBox(), RoomBox(), FxBox()],
     ));
   }
 }
 
-class GridBox extends StatefulWidget {
-  const GridBox({Key? key}) : super(key: key);
+class AllBox extends StatefulWidget {
+  const AllBox({Key? key}) : super(key: key);
 
   @override
-  State<GridBox> createState() => _GridBox();
+  State<AllBox> createState() => _AllBox();
 }
 
-class _GridBox extends State<GridBox> {
+class _AllBox extends State<AllBox> {
   @override
   Widget build(BuildContext context) {
     return GridView.count(
@@ -49,6 +49,96 @@ class _GridBox extends State<GridBox> {
                   ),
                   Text(iotDB[index]['deviceName'] as String),
                   Icon(iotDB[index]['status'] as bool
+                      ? Icons.toggle_off
+                      : Icons.toggle_on)
+                ],
+              ));
+        }));
+  }
+}
+
+class RoomBox extends StatefulWidget {
+  const RoomBox({Key? key}) : super(key: key);
+
+  @override
+  State<RoomBox> createState() => _RoomBox();
+}
+
+class _RoomBox extends State<RoomBox> {
+  final _roomList = iotDB.getRange(0, 3);
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+        shrinkWrap: true,
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        childAspectRatio: 2.0,
+        children: List.generate(_roomList.length, (index) {
+          return Card(
+              margin: const EdgeInsets.all(0),
+              elevation: 5.0,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      setState(() =>
+                          _roomList.elementAt(index)['status'] as bool != true
+                              ? _roomList.elementAt(index)['status'] = true
+                              : _roomList.elementAt(index)['status'] = false);
+                    },
+                    icon: Icon(_roomList.elementAt(index)['icon'] as IconData,
+                        size: 40),
+                  ),
+                  Text(_roomList.elementAt(index)['deviceName'] as String),
+                  Icon(_roomList.elementAt(index)['status'] as bool
+                      ? Icons.toggle_off
+                      : Icons.toggle_on)
+                ],
+              ));
+        }));
+  }
+}
+
+class FxBox extends StatefulWidget {
+  const FxBox({Key? key}) : super(key: key);
+
+  @override
+  State<FxBox> createState() => _FxBox();
+}
+
+class _FxBox extends State<FxBox> {
+  final _fxList = iotDB.getRange(3, 6);
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+        shrinkWrap: true,
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        childAspectRatio: 2.0,
+        children: List.generate(_fxList.length, (index) {
+          return Card(
+              margin: const EdgeInsets.all(0),
+              elevation: 5.0,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      setState(() =>
+                          _fxList.elementAt(index)['status'] as bool != true
+                              ? _fxList.elementAt(index)['status'] = true
+                              : _fxList.elementAt(index)['status'] = false);
+                    },
+                    icon: Icon(_fxList.elementAt(index)['icon'] as IconData,
+                        size: 40),
+                  ),
+                  Text(_fxList.elementAt(index)['deviceName'] as String),
+                  Icon(_fxList.elementAt(index)['status'] as bool
                       ? Icons.toggle_off
                       : Icons.toggle_on)
                 ],
